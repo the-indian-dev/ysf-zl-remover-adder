0a1d4749a6cb3d811d012859c7c761dd2fdf825a
Ritabrata Das <70189264+the-indian-dev@users.noreply.github.com>
2021-03-15 20:48:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ print("Programmed by @the_indian_dev")
 import os
 directory = os.listdir('./input')
 for old in directory:
+    print("Now working on {} file.".format(old))
     f = open('./input/{}'.format(old),'r')
     lst = []
     for line in f:
         if 'ZL' in line:
-            line = line.replace(word,'')
-        lst.append(line)
+            line = line.replace("ZL",'')
+            print("Removing ZL line from {}".format(old))
+            lst.append(line)
         elif '     ' in line:
-            line = line.replace(word,'ZL')
-        lst.append(line)
+            line = line.replace("     ",'ZL')
+            print("Now adding ZL line to {}".format(old))
+            lst.append(line)
     f.close()
     old_name,old_ext = tuple(old.split("."))
     f = open('./output/{}.dnm'.format(old_name),'w')
